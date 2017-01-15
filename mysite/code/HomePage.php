3e8009e517c75eb324f05dae5c69f959b51585c7
Guanzhou Zhao <benzhao2866@gmail.com>
2017-01-15 18:52:58 +1300

    Add management interface using ModelAdmin

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -10,4 +10,12 @@ class HomePage_Controller extends Page_Controller {
       ->sort('Created', 'DESC')
       ->limit($count);
   }
+
+  public function FeaturedProperties() {
+    return Property::get()
+                    ->filter(array(
+                        'FeaturedOnHomepage' => true
+                    ))
+                    ->limit(6);
+  }
 }
